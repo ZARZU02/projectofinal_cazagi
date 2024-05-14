11c6f72db12ab61d30cb05c4464272742a903b27
ZARZU02 <czarzuelagil@gmail.com>
2024-05-14 13:05:39 +0200

    he realizado   cambios  en  el  apartado  de  entrenadores  i  he  realizado  cambios  en  el  estilo  de  la  web

---
+++ backend/src/Entity/Entrenadores.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EntrenadoresRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EntrenadoresRepository::class)]
+class Entrenadores
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nombre = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $apellidos = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $especialidad = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): static
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getApellidos(): ?string
+    {
+        return $this->apellidos;
+    }
+
+    public function setApellidos(string $apellidos): static
+    {
+        $this->apellidos = $apellidos;
+
+        return $this;
+    }
+
+    public function getEspecialidad(): ?string
+    {
+        return $this->especialidad;
+    }
+
+    public function setEspecialidad(string $especialidad): static
+    {
+        $this->especialidad = $especialidad;
+
+        return $this;
+    }
+}
