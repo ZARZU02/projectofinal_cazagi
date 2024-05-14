17aaf22ad88abb141184fd1ba54db3354f699c4c
ZARZU02 <czarzuelagil@gmail.com>
2024-05-14 13:58:30 +0200

    he realizado  cambios  en los entrenadores

--- backend/migrations/Version20240514113703.php
+++ backend/migrations/Version20240514113703.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240514094827 extends AbstractMigration
+final class Version20240514113703 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -25,6 +25,7 @@ final class Version20240514094827 extends AbstractMigration
         $this->addSql('CREATE TABLE clases_alumnos (clases_id INT NOT NULL, alumnos_id INT NOT NULL, INDEX IDX_D0360C9E158CCF68 (clases_id), INDEX IDX_D0360C9EA03F5ABF (alumnos_id), PRIMARY KEY(clases_id, alumnos_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE consejos (id INT AUTO_INCREMENT NOT NULL, titulo VARCHAR(255) NOT NULL, descripcion VARCHAR(10000) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE contactos (id INT AUTO_INCREMENT NOT NULL, nombre VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE entrenadores (id INT AUTO_INCREMENT NOT NULL, nombre VARCHAR(255) NOT NULL, apellidos VARCHAR(255) NOT NULL, especialidad VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, direccion VARCHAR(255) NOT NULL, telefono INT NOT NULL, nombre VARCHAR(255) NOT NULL, apellidos VARCHAR(255) NOT NULL, deportes VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE clases_alumnos ADD CONSTRAINT FK_D0360C9E158CCF68 FOREIGN KEY (clases_id) REFERENCES clases (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE clases_alumnos ADD CONSTRAINT FK_D0360C9EA03F5ABF FOREIGN KEY (alumnos_id) REFERENCES alumnos (id) ON DELETE CASCADE');
@@ -40,6 +41,7 @@ final class Version20240514094827 extends AbstractMigration
         $this->addSql('DROP TABLE clases_alumnos');
         $this->addSql('DROP TABLE consejos');
         $this->addSql('DROP TABLE contactos');
+        $this->addSql('DROP TABLE entrenadores');
         $this->addSql('DROP TABLE user');
     }
 }
--- backend/src/Controller/LoginController.php
+++ backend/src/Controller/LoginController.php
@@ -12,23 +12,22 @@ class LoginController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        if ($this->getUser()) {
-            return $this->redirectToRoute('app_inicio');
-        }
-        
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
 
+        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('login/login.html.twig', [
-            'last_username' => $lastUsername,
-            'error' => $error,
-        ]);
+        return $this->render('login/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
-    #[Route('/logout', name: 'app_logout')]
-    public function logout(): void
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): Response
     {
-        // controller can be blank: it will never be executed!
-        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        return $this->redirectToRoute('app_inicio');
+        //throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
